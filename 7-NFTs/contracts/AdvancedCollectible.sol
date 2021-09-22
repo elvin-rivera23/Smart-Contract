db1ece0acd0031bf1d91741e46472c22672d8dc2
Elvin Rivera <elvinkrivera@gmail.com>
2021-09-21 19:52:49 -0700

    add notes to NFTs

--- 7-NFTs/contracts/AdvancedCollectible.sol
+++ 7-NFTs/contracts/AdvancedCollectible.sol
@@ -6,46 +6,66 @@
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol"; // provably random nft
 
 contract AdvancedCollectible is ERC721, VRFConsumerBase {
     uint256 public tokenCounter;
     bytes32 public keyhash;
     uint256 public fee;
-    enum Breed{PUG, SHIBA_INU, ST_BERNARD}
-    mapping(uint256 => Breed) public tokenIdToBreed;
-    mapping(bytes32 => address) public requestIdToSender;
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
+    mapping(uint256 => Breed) public tokenIdToBreed; // fulfillRandomness
+    mapping(bytes32 => address) public requestIdToSender; // request sender as key and whoever sends it as value
     event requestedCollectible(bytes32 indexed requestId, address requester);
     event breedAssigned(uint256 indexed tokenId, Breed breed);
 
-    constructor(address _vrfCoordinator, address _linkToken, bytes32 _keyhash, uint256 _fee) public 
-    VRFConsumerBase(_vrfCoordinator, _linkToken)
-    ERC721("Dogie", "DOG")
+    // parameters, double inherited constructor
+    constructor(
+        address _vrfCoordinator,
+        address _linkToken,
+        bytes32 _keyhash,
+        uint256 _fee
+    )
+        public
+        VRFConsumerBase(_vrfCoordinator, _linkToken)
+        ERC721("Dogie", "DOG")
     {
         tokenCounter = 0;
         keyhash = _keyhash;
         fee = _fee;
     }
 
-    function createCollectible() public returns (bytes32){
-        bytes32 requestId = requestRandomness(keyhash, fee);
+    function createCollectible() public returns (bytes32) {
+        bytes32 requestId = requestRandomness(keyhash, fee); //get random breed for dogs
         requestIdToSender[requestId] = msg.sender;
-        emit requestedCollectible(requestId, msg.sender);
+        emit requestedCollectible(requestId, msg.sender); // emit event for mapping update
     }
 
-    function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
-        Breed breed = Breed(randomNumber % 3);
+    // only internal vrf can call this
+    function fulfillRandomness(bytes32 requestId, uint256 randomNumber)
+        internal
+        override
+    {
+        Breed breed = Breed(randomNumber % 3); // breed is of type Breed
+        // create mapping from breed to TokenID
         uint256 newTokenId = tokenCounter;
         tokenIdToBreed[newTokenId] = breed;
         emit breedAssigned(newTokenId, breed);
         address owner = requestIdToSender[requestId];
-        _safeMint(owner, newTokenId);
+        _safeMint(owner, newTokenId); // vrf coordinator is the one calling mint, not sender
         tokenCounter = tokenCounter + 1;
     }
 
+    //TokenID will update TokenURI
     function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
-        // pug, shiba inu, st bernard
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not owner no approved");
+        // pug, shiba inu, st bernard ; open zepplin function (require)
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: caller is not owner no approved"
+        );
         _setTokenURI(tokenId, _tokenURI);
     }
 }
--- 7-NFTs/contracts/SimpleCollectible.sol
+++ 7-NFTs/contracts/SimpleCollectible.sol
@@ -1,20 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.6;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; //include in yaml
 
 contract SimpleCollectible is ERC721 {
-    uint256 public tokenCounter;
-    
-    constructor () public ERC721 ("Dogie", "DOG"){
+    uint256 public tokenCounter; // global (constructor, createCollectible)
+
+    // name and symbol
+    constructor() public ERC721("Dogie", "DOG") {
         tokenCounter = 0;
     }
 
-    function createCollectible(string memory tokenURI) public returns (uint256){
+    // create new nft and assign to whover called this function
+    // mapping tokenID to new address/owner
+    function createCollectible(string memory tokenURI)
+        public
+        returns (uint256)
+    {
         uint256 newTokenId = tokenCounter;
-        _safeMint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, tokenURI);
+        // inherit function from openzeppelin: https://docs.openzeppelin.com/contracts/2.x/erc721
+        _safeMint(msg.sender, newTokenId); // owner, unique tokenID
+        _setTokenURI(newTokenId, tokenURI); // uri https: uniquely points to metadata file, immage associated to it that you can see
         tokenCounter = tokenCounter + 1;
         return newTokenId;
     }
 }
+
+// NOTES: safemint checks to see if tokenID has been used
